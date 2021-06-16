cb4b3719b13504b0f47618cf79082f3c4a959048
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-06-16 18:54:39 +0800

    1. add edu config

--- config/conf.js
+++ config/conf.js
@@ -1,16 +1,16 @@
 if (process.env.NODE_ENV == 'development') { //开发环境
   module.exports =  {
-    title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
+    title: '七橙-打造全行业都适用的在线教育系统',
     baseUrl: 'https://edu-api.7otech.com'   //java项目接口地址
   }
 }else if (process.env.NODE_ENV == 'testing') {  //测试环境
   module.exports = {
-    title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
-    baseUrl: 'http://127.0.0.1:5840'  //java项目接口地址
+    title: '七橙-打造全行业都适用的在线教育系统',
+    baseUrl: 'https://edu-api.7otech.com'  //java项目接口地址
   }
 }else { //默认生产环境
   module.exports = {
-    title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
-    baseUrl: 'http://127.0.0.1:5840'   //java项目接口地址
+    title: '七橙-打造全行业都适用的在线教育系统',
+    baseUrl: 'https://edu-api.7otech.com'   //java项目接口地址
   }
 }
